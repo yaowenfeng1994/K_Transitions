42a5327c54637445623412dc0a701a2cd3b014af
twist <610733719@qq.com>
2019-07-19 18:23:57 +0800

    fix

--- transitions/machine_test.go
+++ transitions/machine_test.go
@@ -10,15 +10,13 @@ import (
 const (
 	Initial  = "Initial"
 	WaitPay  = "WaitPay"
-	Paying   = "Paying"
-	WaitSend = "WaitSend"
+	PaySuccess   = "PaySuccess"
 )
 
 // 动作
 const (
 	CreateEvent     = "Create"
 	PayEvent        = "Pay"
-	PaySuccessEvent = "PaySuccess"
 )
 
 func doAction(ctx context.Context, from State, event Event, to []State) (state State, e error) {
@@ -28,19 +26,17 @@ func doAction(ctx context.Context, from State, event Event, to []State) (state S
 
 var Transitions = []Transition{
 	{Initial, CreateEvent, []State{WaitPay}, doAction, nil},
-	{WaitPay, PayEvent, []State{Paying}, doAction, nil},
-	{Paying, PaySuccessEvent, []State{WaitSend}, doAction, nil},
+	{WaitPay, PayEvent, []State{PaySuccess}, doAction, nil},
 }
 
 var states = StatesDef{
 	Initial: "开始",
 	WaitPay: "待支付",
-	Paying:  "支付中",}
+	PaySuccess:  "支付成功",}
 
 var events = EventsDef{
 	CreateEvent:     "创建订单",
-	PayEvent:        "支付",
-	PaySuccessEvent: "支付成功",}
+	PayEvent:        "支付",}
 
 type OrderEventProcessor struct{}
 
@@ -65,13 +61,14 @@ func TestStateMachine_Example_Order(t *testing.T) {
 	orderStateMachine.States(states)
 	orderStateMachine.Events(events)
 	orderStateMachine.Processor(&OrderEventProcessor{})
-	order := context.WithValue(context.TODO(), "data", "order object data")
-	state, err := orderStateMachine.Trigger(order, Initial, CreateEvent)
-	println(fmt.Sprintf("====: %v : %v", state, err))
+	//order := context.WithValue(context.TODO(), "data", "order object data")
+
+	//state, err := orderStateMachine.Trigger(order, Initial, CreateEvent)
+	//println(fmt.Sprintf("====: %v : %v", state, err))
 
 	//state, err = orderStateMachine.Trigger(order, Initial, PayEvent)
 	//println(fmt.Sprintf("====: %v : %v", state, err))
 
-	state, err = orderStateMachine.Trigger(order, WaitPay, PayEvent)
-	println(fmt.Sprintf("====: %v : %v", state, err))
+	//state, err = orderStateMachine.Trigger(order, WaitPay, PayEvent)
+	//println(fmt.Sprintf("====: %v : %v", state, err))
 }
